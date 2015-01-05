04661ef490139e33e5c169353be860fa63fd4f20
Philipp Janda <siffiejoe@gmx.net>
2015-01-05 01:34:48 +0100

    fixed tests to avoid module function on new Lua versions

--- tests/module2.lua
+++ tests/module2.lua
@@ -1,4 +1,10 @@
-module( "module2" )
+if _VERSION == "Lua 5.1" then
+  module( "module2" )
+else
+  local _M = {}
+  package.loaded[ "module2" ] = _M
+  _ENV = _M
+end
 
 function func()
   return "module2"

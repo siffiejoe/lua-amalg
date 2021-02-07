536ff56b33320e672550b823d837db51c17fe454
Philipp Janda <siffiejoe@gmx.net>
2021-02-07 11:08:47 +0100

    Test byte reads in virtual IO

--- tests/vio.lua
+++ tests/vio.lua
@@ -9,7 +9,8 @@ hello world
 
 local file = assert( io.open( "data.txt", "r" ) )
 
-assert( "hello world" == file:read( "*l" ) )
+assert( "hello " == file:read( 6 ) )
+assert( "world" == file:read( "*l" ) )
 assert( "hello world\n" == file:read( "*L" ) )
 assert( 1 == file:read( "*n" ) )
 assert( -2.5 == file:read( "*n" ) )
@@ -19,30 +20,31 @@ assert( 10 == file:read( "*n" ) )
 assert( 0 == file:seek( "set" ) )
 assert( contents == file:read( "*a" ) )
 assert( 0 == file:seek( "set" ) )
-local function checkvalues( a, b, c, d, e, f, g )
-  assert( a == "hello world" )
-  assert( b == "hello world\n" )
-  assert( c == 1 )
-  assert( d == -2.5 )
-  assert( e == 3735928559 )
-  assert( f == 15 )
-  assert( g == 10 )
+local function checkvalues( a, b, c, d, e, f, g, h )
+  assert( a == "hello " )
+  assert( b == "world" )
+  assert( c == "hello world\n" )
+  assert( d == 1 )
+  assert( e == -2.5 )
+  assert( f == 3735928559 )
+  assert( g == 15 )
+  assert( h == 10 )
   return true
 end
-local a1, b1, c1, d1, e1, f1, g1 = file:read( "l", "L", "n", "n", "n", "n", "n" )
-checkvalues( a1, b1, c1, d1, e1, f1, g1 )
+local a1, b1, c1, d1, e1, f1, g1, h1 = file:read( 6, "l", "L", "n", "n", "n", "n", "n" )
+checkvalues( a1, b1, c1, d1, e1, f1, g1, h1 )
 assert( 0 == file:seek( "set" ) )
 local looped = false
-for a2, b2, c2, d2, e2, f2, g2 in file:lines( "l", "L", "n", "n", "n", "n", "n" ) do
-  if checkvalues( a2, b2, c2, d2, e2, f2, g2 ) then
+for a2, b2, c2, d2, e2, f2, g2, h2 in file:lines( 6, "l", "L", "n", "n", "n", "n", "n" ) do
+  if checkvalues( a2, b2, c2, d2, e2, f2, g2, h2 ) then
     looped = true
     break
   end
 end
 assert( looped )
 looped = false
-for a2, b2, c2, d2, e2, f2, g2 in io.lines( "data.txt", "l", "L", "n", "n", "n", "n", "n" ) do
-  if checkvalues( a2, b2, c2, d2, e2, f2, g2 ) then
+for a2, b2, c2, d2, e2, f2, g2, h2 in io.lines( "data.txt", 6, "l", "L", "n", "n", "n", "n", "n" ) do
+  if checkvalues( a2, b2, c2, d2, e2, f2, g2, h2 ) then
     looped = true
     break
   end

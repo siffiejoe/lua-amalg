029a05db7a68d0a0031b718e30c67915d65463e7
Philipp Janda <siffiejoe@gmx.net>
2021-05-09 19:00:51 +0200

    Fix "issues" flagged by cpplint

--- tests/aiomod.c
+++ tests/aiomod.c
@@ -1,31 +1,32 @@
+/* Copyright 2021 Philipp Janda */
 #include <lua.h>
 #include <lauxlib.h>
 
 
-static int l_func1( lua_State* L ) {
-  lua_pushliteral( L, "aiomodule1" );
+static int l_func1(lua_State* L) {
+  lua_pushliteral(L, "aiomodule1");
   return 1;
 }
 
 
-static int l_func2( lua_State* L ) {
-  lua_pushliteral( L, "aiomodule2" );
+static int l_func2(lua_State* L) {
+  lua_pushliteral(L, "aiomodule2");
   return 1;
 }
 
 
-int luaopen_aiomod_a( lua_State* L ) {
-  lua_newtable( L );
-  lua_pushcfunction( L, l_func1 );
-  lua_setfield( L, -2, "func" );
+int luaopen_aiomod_a(lua_State* L) {
+  lua_newtable(L);
+  lua_pushcfunction(L, l_func1);
+  lua_setfield(L, -2, "func");
   return 1;
 }
 
 
-int luaopen_aiomod_b( lua_State* L ) {
-  lua_newtable( L );
-  lua_pushcfunction( L, l_func2 );
-  lua_setfield( L, -2, "func" );
+int luaopen_aiomod_b(lua_State* L) {
+  lua_newtable(L);
+  lua_pushcfunction(L, l_func2);
+  lua_setfield(L, -2, "func");
   return 1;
 }
 
--- tests/cmod.c
+++ tests/cmod.c
@@ -1,16 +1,17 @@
+/* Copyright 2021 Philipp Janda */
 #include <lua.h>
 #include <lauxlib.h>
 
 
-static int l_func( lua_State* L ) {
-  lua_pushliteral( L, "cmodule" );
+static int l_func(lua_State* L) {
+  lua_pushliteral(L, "cmodule");
   return 1;
 }
 
-int luaopen_cmod( lua_State* L ) {
-  lua_newtable( L );
-  lua_pushcfunction( L, l_func );
-  lua_setfield( L, -2, "func" );
+int luaopen_cmod(lua_State* L) {
+  lua_newtable(L);
+  lua_pushcfunction(L, l_func);
+  lua_setfield(L, -2, "func");
   return 1;
 }
 

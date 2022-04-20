2a6543f7471fed87e03bbd895b2757ed8011358b
Philipp Janda <siffiejoe@gmx.net>
2022-04-20 11:40:19 +0200

    Provide path for error messages

--- src/amalg/dumbluaparser/transform.lua
+++ src/amalg/dumbluaparser/transform.lua
@@ -1,8 +1,8 @@
 local parser = require( "dumbParser" )
 
-return function( s, is_text )
+return function( s, is_text, path )
   if is_text then
-    local tokens = assert( parser.tokenize( s ) )
+    local tokens = assert( parser.tokenize( s, path ) )
     local ast = assert( parser.parse( tokens ) )
     parser.minify( ast, true )
     return assert( parser.toLua( ast ) ), true
